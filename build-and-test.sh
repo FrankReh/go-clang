928af2eb977811e721f5d401e056bba53daf417a
Frank Rehwinkel <frankrehwinkel@gmail.com>
2018-02-27 21:20:09 -0500

    script for building and testing in one shot

---
+++ build-and-test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+set -x
+
+CGO_LDFLAGS=
+case $(uname) in
+    Darwin)
+        DYLD_LIBRARY_PATH=
+        ;;
+    *)
+        LD_LIBRARY_PATH=
+        ;;
+esac
+
+. env.sh
+
+cd clang
+go install
+go test
+
+cd ../cmd/go-clang-dump
+go build
+go test
+
+cd ../go-clang-compdb
+go build
+go test
+
+cd ../go-clang-includes
+go build
+go test
