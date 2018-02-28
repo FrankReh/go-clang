a9e43e93d9e74bdc8e1edff2c44e48866bdcf5ce
Frank Rehwinkel <frankrehwinkel@gmail.com>
2018-02-28 18:08:15 -0500

    cleanup Result
    Fix the constants so they are all of the proper type.
    
    Remove Spelling() for this type, just use String().

--- clang/result_gen.go
+++ clang/result_gen.go
@@ -10,25 +10,23 @@ type Result uint32
 const (
 	// Function returned successfully.
 	Result_Success Result = C.CXResult_Success
+
 	// One of the parameters was invalid for the function.
-	Result_Invalid = C.CXResult_Invalid
+	Result_Invalid Result = C.CXResult_Invalid
+
 	// The function was terminated by a callback (e.g. it returned CXVisit_Break)
-	Result_VisitBreak = C.CXResult_VisitBreak
+	Result_VisitBreak Result = C.CXResult_VisitBreak
 )
 
-func (r Result) Spelling() string {
+func (r Result) String() string {
 	switch r {
 	case Result_Success:
-		return "Result=Success"
+		return "Result_Success"
 	case Result_Invalid:
-		return "Result=Invalid"
+		return "Result_Invalid"
 	case Result_VisitBreak:
-		return "Result=VisitBreak"
+		return "Result_VisitBreak"
 	}
 
-	return fmt.Sprintf("Result unkown %d", int(r))
-}
-
-func (r Result) String() string {
-	return r.Spelling()
+	return fmt.Sprintf("Result unknown %d", int(r))
 }
