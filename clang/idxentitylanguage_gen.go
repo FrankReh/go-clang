64af4be64415878a8fa91d059bcef7d698bbc5ad
Frank Rehwinkel <frankrehwinkel@gmail.com>
2018-02-28 23:39:17 -0500

    cleanup IdxEntityLanguage
    Fix the constants so they are all of the proper type.
    
    Remove Spelling() for this type, just use String().

--- clang/idxentitylanguage_gen.go
+++ clang/idxentitylanguage_gen.go
@@ -9,29 +9,25 @@ type IdxEntityLanguage uint32
 
 const (
 	IdxEntityLang_None  IdxEntityLanguage = C.CXIdxEntityLang_None
-	IdxEntityLang_C                       = C.CXIdxEntityLang_C
-	IdxEntityLang_ObjC                    = C.CXIdxEntityLang_ObjC
-	IdxEntityLang_CXX                     = C.CXIdxEntityLang_CXX
-	IdxEntityLang_Swift                   = C.CXIdxEntityLang_Swift
+	IdxEntityLang_C     IdxEntityLanguage = C.CXIdxEntityLang_C
+	IdxEntityLang_ObjC  IdxEntityLanguage = C.CXIdxEntityLang_ObjC
+	IdxEntityLang_CXX   IdxEntityLanguage = C.CXIdxEntityLang_CXX
+	IdxEntityLang_Swift IdxEntityLanguage = C.CXIdxEntityLang_Swift
 )
 
-func (iel IdxEntityLanguage) Spelling() string {
+func (iel IdxEntityLanguage) String() string {
 	switch iel {
 	case IdxEntityLang_None:
-		return "IdxEntityLang=None"
+		return "IdxEntityLang_None"
 	case IdxEntityLang_C:
-		return "IdxEntityLang=C"
+		return "IdxEntityLang_C"
 	case IdxEntityLang_ObjC:
-		return "IdxEntityLang=ObjC"
+		return "IdxEntityLang_ObjC"
 	case IdxEntityLang_CXX:
-		return "IdxEntityLang=CXX"
+		return "IdxEntityLang_CXX"
 	case IdxEntityLang_Swift:
-		return "IdxEntityLang=Swift"
+		return "IdxEntityLang_Swift"
 	}
 
 	return fmt.Sprintf("IdxEntityLanguage unkown %d", int(iel))
-}
-
-func (iel IdxEntityLanguage) String() string {
-	return iel.Spelling()
 }
